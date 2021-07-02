17ed6fa4e3ed59d666667e6cb5db2fc678476fef
apoorvasanikop <apoorva.sanikop98@gmail.com>
2021-07-02 09:40:13 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MainPageComponent } from './main-page/main-page.component';
+import { ElectionofficerComponent} from './electionofficer/electionofficer.component';
+import {VoterComponent} from './voter/voter.component';
+import {AdminComponent} from './admin/admin.component';
+const routes: Routes = [
+  {path: 'main-page', component: MainPageComponent},
+  {path: 'electionofficer-page', component: ElectionofficerComponent},
+  {path: 'voter-page', component: VoterComponent},
+  {path: 'admin-page', component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ElectionofficerComponent } from './electionofficer/electionofficer.component';
+import { VoterComponent } from './voter/voter.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ElectionofficerComponent,
+    VoterComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  exports:[
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
